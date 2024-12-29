7aadc7b5d3e049eccd7ea097cff5783466008601
vngthb <nvswrk@gmail.com>
2024-12-29 13:54:07 +0200

    make factor configurable

--- src/tryaga.erl
+++ src/tryaga.erl
@@ -1,28 +1,32 @@
 -module(tryaga).
 
+-define(DEFAULT_FACTOR, 0.66).
+
 -export([
-    retry/4
+    retry/4,
+    retry/5
 ]).
 
+retry(Function, Predicate, Attempts, Duration) ->
+    retry(Function, Predicate, Attempts, Duration, ?DEFAULT_FACTOR).
 
-retry(Function, Predicate, Attempts, Duration0) ->
-    GrowthRatio = 0.66,
-    Duration1 = Duration0 * (1 - GrowthRatio) / (1 - math:pow(GrowthRatio, Attempts)),
-    retry(Function, Predicate, Attempts, Duration1, GrowthRatio).
+retry(Function, Predicate, Attempts, Duration, Factor) ->
+    Base = Duration * (1 - Factor) / (1 - math:pow(Factor, Attempts)),
+    retry(internal, Function, Predicate, Attempts, Base, Factor).
 
-retry(Function, Predicate, 0, _, _) ->
+retry(internal, Function, Predicate, 0, _, _) ->
     Result = Function(),
     case Predicate(Result) of
         true -> {ok, Result};
         false -> {error, Result}
     end;
-retry(Function, Predicate, Attempts, Duration, GrowthRatio) ->
+retry(internal, Function, Predicate, Attempts, Duration, Factor) ->
     Result = Function(),
     case Predicate(Result) of
         true -> {ok, Result};
         false ->
-            Sleep0 = Duration * math:pow(GrowthRatio, Attempts - 1),
-            Sleep1 = round(Sleep0),
-            timer:sleep(Sleep1),
-            retry(Function, Predicate, Attempts - 1, Duration, GrowthRatio)
+            Timeout0 = Duration * math:pow(Factor, Attempts - 1),
+            Timeout1 = round(Timeout0),
+            timer:sleep(Timeout1),
+            retry(internal, Function, Predicate, Attempts - 1, Duration, Factor)
     end.
