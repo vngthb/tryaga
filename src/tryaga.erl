15045e77a4101200c34385934a90f5d9aff65848
vngthb <nvswrk@gmail.com>
2024-12-29 17:14:11 +0200

    simplify tryaga:retry/6

--- src/tryaga.erl
+++ src/tryaga.erl
@@ -18,17 +18,13 @@ retry(Function, Predicate, Retries, Duration, Factor) ->
     Base = Duration * (1 - Factor) / (1 - math:pow(Factor, Retries)),
     retry(internal, Function, Predicate, Retries, Base, Factor).
 
-retry(internal, Function, Predicate, 0, _, _) ->
-    Result = Function(),
-    case Predicate(Result) of
-        true -> {ok, Result};
-        false -> {error, Result}
-    end;
 retry(internal, Function, Predicate, Retries, Duration, Factor) ->
     Result = Function(),
-    case Predicate(Result) of
-        true -> {ok, Result};
-        false ->
+    Expected = Predicate(Result),
+    if
+        Expected == true -> {ok, Result};
+        Retries == 0 -> {error, Result};
+        true -> 
             Timeout0 = Duration * math:pow(Factor, Retries - 1),
             Timeout1 = round(Timeout0),
             timer:sleep(Timeout1),
